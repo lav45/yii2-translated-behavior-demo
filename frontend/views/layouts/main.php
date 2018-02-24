73a1fca74b25d851be517f9430c1c65b14e89ed3
LAV45 <LAV451@gmail.com>
2018-02-25 01:18:53 +0300

    Update composer package; Add Yandex.Metrika

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -105,6 +105,21 @@ $gitHubLogo = '<svg height="16" class="octicon" version="1.1" viewBox="0 0 16 16
     </div>
 </footer>
 
+<!-- Yandex.Metrika counter -->
+<script src="https://mc.yandex.ru/metrika/watch.js" type="text/javascript"></script>
+<script type="text/javascript" >
+    try {
+        var yaCounter47817976 = new Ya.Metrika({
+            id:47817976,
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true
+        });
+    } catch(e) { }
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/47817976" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 <?php $this->endBody() ?>
 </body>
 </html>
